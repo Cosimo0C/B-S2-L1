611b73a00776a32898e1506593c39b0315d055b1
Cosimo0C <cosimocrupi01@gmail.com>
2025-06-23 17:36:03 +0200

    a

---
+++ src/Es1/Main.java
@@ -0,0 +1,41 @@
+package Es1;
+
+import PackExceptions.OverPositionException;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        try {
+            int pos = 0;
+            do {
+                int [] las =new int [5];
+                for (int i = 0; i < las.length; i++) {
+                    Random random=new Random();
+                    las[i]= random.nextInt(10)+1;
+                    System.out.print(las[i]+ " ");
+                }
+                System.out.println();
+                System.out.println("In che posizione vorresti accedere? Scegli da 1 a 5. 0 per uscire!");
+                pos= scanner.nextInt();
+                if (pos>0 & pos<= las.length){
+                    System.out.println("Che numero vorresti inserire nella posizione scelta?");
+                    int num= scanner.nextInt();
+                    pos=pos-1;
+                    las[pos]=num;
+                    for (int j = 0; j < las.length; j++) {
+                        System.out.print(las[j]+" ");
+                    }
+                    System.out.println();
+                } else if (pos==0) {
+                    System.out.println("Fine!");
+                }else {
+                    throw new OverPositionException();
+                }
+            }while (pos!=0);
+        }catch (OverPositionException ex){
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        try {
-            int pos = 0;
-            do {
-                int [] las =new int [5];
-                for (int i = 0; i < las.length; i++) {
-                    Random random=new Random();
-                    las[i]= random.nextInt(10)+1;
-                    System.out.println(las[i]);
-                }
-                System.out.println("In che posizione vorresti accedere? Scegli da 1 a 5. 0 per uscire!");
-                pos= scanner.nextInt();
-                if (pos>0 & pos<= las.length){
-                    System.out.println("Che numero vorresti inserire nella posizione scelta?");
-                    int num= scanner.nextInt();
-                    pos=pos-1;
-                    las[pos]=num;
-                    for (int j = 0; j < las.length; j++) {
-                        System.out.println(las[j]);
-                    }
-                } else if (pos==0) {
-                    System.out.println("Fine!");
-                }else {
-                    throw new OverPositionException();
-                }
-            }while (pos!=0);
-        }catch (OverPositionException ex){
-        }
+
     }
 }
